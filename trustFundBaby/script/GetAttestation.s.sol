cbd39c56a2ab0af3c8cd63733148511459a8a7b6
Rick <rick@portto.com>
2023-11-18 15:13:56 +0300

    compileable & able to get attestation record

--- trustFundBaby/script/GetAttestation.s.sol
+++ trustFundBaby/script/GetAttestation.s.sol
@@ -19,17 +19,57 @@ import {
     RevocationRequestData
 } from "../src/IEAS.sol";
 
+import {
+    ISchemaRegistry,
+    SchemaRecord
+} from "../src/ISchemaRegistry.sol";
+
 contract GetAttestation is Script {
     function run() external {
-        vm.startBroadcast();
+        // vm.startBroadcast();
+        
         bytes32 uid = 0xdd1ba51844b60276294490c24d3d272625c68868dba835ac9602c65a26149791;
-        Attestation att = getAttestation();
-        console2.log("Attestation: %s", att);
-        vm.stopBroadcast();
-        return wallet;
+        IEAS eas = IEAS(address(0x4200000000000000000000000000000000000021));
+        Attestation memory att = eas.getAttestation(uid);
+        console2.log("Attestation: %s", string(abi.encodePacked(att.uid)));
+        console2.log("Attestation ts: %s", att.time);
+
+        ISchemaRegistry registry = ISchemaRegistry(address(0x4200000000000000000000000000000000000020));
+        bytes32 suid = 0xbe92c980d4766a5d83d746cfec0b5eccc79f925e42d9215159f10c34ac6825fa;
+        SchemaRecord memory schema = registry.getSchema(suid);
+        console2.log("Schema: %s", string(abi.encodePacked(schema.uid)));
+
+        console2.log("TEST: %s", bytes32ToString(schema.uid));
+        console2.log("TEST revocable: %s", (schema.revocable));
+
+        // vm.stopBroadcast();
+        return;
     }
 
-    function getAttestation(bytes32 uid) external view returns (Attestation memory) {
-        return _db[uid];
+
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+    // Temporary memory buffer
+    bytes memory buffer = new bytes(32);
+    uint256 charCount = 0;
+
+    // Copy only non-zero bytes
+    for (uint256 i = 0; i < 32; i++) {
+        if (_bytes32[i] != 0) {
+            buffer[charCount] = _bytes32[i];
+            charCount++;
+        } else {
+            break;
+        }
     }
+
+    bytes memory bytesBuffer = new bytes(charCount);
+    for (uint256 i = 0; i < charCount; i++) {
+        bytesBuffer[i] = buffer[i];
+    }
+
+    return string(bytesBuffer);
 }
+
+
+}
+
--- trustFundBaby/src/resolver/SchemaResolver.sol
+++ trustFundBaby/src/resolver/SchemaResolver.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import { IEAS, Attestation } from "../IEAS.sol";
 import { AccessDenied, InvalidEAS, InvalidLength, uncheckedInc } from "../Common.sol";
